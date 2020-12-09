4f13bb883152d910c9e22c6280b6f7c5f3ed262b
davekch <dave-koch@web.de>
2020-12-09 01:13:17 +0100

    haskell: add bootloader to util

--- utils/Utils.hs
+++ utils/Utils.hs
@@ -2,6 +2,10 @@ module Utils
 ( parseInts
 , test
 , groupByEmptyLines
+, Command
+, BootStatus (INFLOOP, DONE)
+, strToCommand
+, bootLoader
 ) where
 
 import Data.Function (on)
@@ -38,3 +42,32 @@ test f expected x
     | otherwise = Fail expected out
     where
         out = f x
+
+        
+        
+-- ========================= bootloader ======================
+data Instruction = NOP | ACC | JMP
+data Command = Command Instruction Int
+data BootStatus = INFLOOP | DONE deriving (Eq)
+
+strToCommand :: String -> Command
+strToCommand command = case com of
+    "nop" -> Command NOP value
+    "acc" -> Command ACC value
+    "jmp" -> Command JMP value
+    where
+        (com:(s:v):_) = words command
+        f = if s == '-' then negate else id  -- check the sign of the integer
+        value = f $ read v
+
+bootLoader' :: Int -> Int -> [Int] -> [Command] -> (BootStatus, Int)
+bootLoader' acc pos history code
+    | pos `elem` history = (INFLOOP, acc)
+    | pos >= length code    = (DONE, acc)
+    | otherwise = case code !! pos of
+        (Command NOP _)     -> bootLoader' acc         (pos+1) (pos:history) code
+        (Command ACC value) -> bootLoader' (acc+value) (pos+1) (pos:history) code
+        (Command JMP value) -> bootLoader' acc     (pos+value) (pos:history) code
+
+bootLoader :: [Command] -> (BootStatus, Int)
+bootLoader = bootLoader' 0 0 []
