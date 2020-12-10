7ea2aae76d323d0d442900a8d16d406e81bc04b1
davekch <dave-koch@web.de>
2020-12-10 11:43:31 +0100

    slight improvement of haskell template

--- templates/solver.hs
+++ templates/solver.hs
@@ -7,29 +7,35 @@ getInput = do
     return contents
 
 
-parse :: String -> a
+type Parsed = [Int]
+type Sol1 = Int
+type Sol2 = Int
+
+
+
+parse :: String -> Parsed
 parse input = undefined
 
 
-solve1 :: a -> String
+solve1 :: Parsed -> Sol1
 solve1 parsed = undefined
 
 
-solve2 :: a -> String
+solve2 :: Parsed -> Sol2
 solve2 parsed = undefined
 
 
 testdata = ""
-testresult1 = ""
-testresult2 = ""
-test1 = test (solve1 . parse) testdata testresult1
-test2 = test (solve2 . parse) testdata testresult2
+testresult1 = 0
+testresult2 = 0
+test1 = test (solve1 . parse) testresult1 testdata
+test2 = test (solve2 . parse) testresult2 testdata
 
 
-main = do 
+main = do
     raw_input <- getInput
     let parsed = parse raw_input
-    let sol1 = solve1 parsed 
-    putStrLn ("Part 1: " ++ sol1)
-    let sol2 = solve2 parsed 
-    putStrLn ("Part 2: " ++ sol2)
+    let sol1 = solve1 parsed
+    putStrLn ("Part 1: " ++ show sol1)
+    let sol2 = solve2 parsed
+    putStrLn ("Part 2: " ++ show sol2)
