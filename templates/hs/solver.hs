2604c9935b5ee0d63c7bd12d5e63e5101bf7200a
David Koch <dave-koch@web.de>
2022-11-28 23:56:40 +0100

    improve haskell template

--- templates/hs/solver.hs
+++ templates/hs/solver.hs
@@ -1,34 +1,31 @@
 import Data.List
-import Utils
+import Utils as U
 import Utils (CLIOptions, clioptions)
 
 getInput :: IO String
-getInput = do
-    contents <- readFile "input.txt"
-    return contents
+getInput = readFile "input.txt"
 
+--------------------------------------------------------------------
 
 type Parsed = [Int]
 type Sol1 = Int
 type Sol2 = Int
 
-
-
 parse :: String -> Parsed
 parse input = undefined
-
 
 solve1 :: Parsed -> Sol1
 solve1 parsed = undefined
 
-
 solve2 :: Parsed -> Sol2
 solve2 parsed = undefined
-
 
 testdata = ""
 testresult1 = 0
 testresult2 = 0
+
+--------------------------------------------------------------------
+
 test1 = test (solve1 . parse) testresult1 testdata
 test2 = test (solve2 . parse) testresult2 testdata
 
@@ -46,7 +43,7 @@ main' (CLIOptions p False) = do
         _ -> (do
             printPart 1 (solve1 parsed)
             printPart 2 (solve2 parsed))
-main' (CLIOptions p True) = do
+main' (CLIOptions p True) = 
     case p of
         1 -> printPart 1 test1
         2 -> printPart 2 test2
