b6df42db66f4104895b8b89612b825a831f152f0
David Koch <dave-koch@web.de>
2022-11-28 23:15:54 +0100

    clean up c++ utilities

--- utils/cpp/Utils.hpp
+++ utils/cpp/Utils.hpp
@@ -28,43 +28,4 @@ namespace util {
 
 }
 
-// here comes stuff that is too specific to be util::
-
-enum Instruction {
-    NOP,
-    ACC,
-    JMP
-};
-
-struct Command {
-    Instruction instruction;
-    int value;
-};
-
-Command str_to_command(std::string input);
-
-enum BootStatus {
-    DONE,
-    INFLOOP
-};
-
-class BootLoader {
-private:
-    std::vector<Command> code;
-    int pos;
-    int acc;
-    std::vector<int> history;
-    // logging utility
-    bool log = false;
-public:
-    BootLoader(std::vector<Command>);
-    void reset();
-    void set_code(std::vector<Command>);
-    BootStatus run();
-    int get_result();
-    void set_command(int, Command);
-    void set_log(bool);
-};
-
-
 #endif
