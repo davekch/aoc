5162584feb5335215b3b36f6133ae6cf32d874f8
davekch <dave-koch@web.de>
2023-11-17 01:09:14 +0100

    julia: add trivial utility functions

--- utils/jl/Utils.jl
+++ utils/jl/Utils.jl
@@ -3,4 +3,15 @@ module Utils
 include("Graphs.jl")
 export Graphs
 
+
+lines(s::AbstractString) = split(strip(s), "\n")
+export lines
+
+int(s::AbstractString)::Int = parse(Int, s)
+export int
+
+ints(s::AbstractString)::Vector{Int} = map(int, [s[idx] for idx in findall(r"-?[0-9]+", s)])
+export ints
+
 end # module Utils
+
