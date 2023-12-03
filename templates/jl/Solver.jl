85a0ff8876dc28c5bb06fcf4fe84233cfcd3e3d9
davekch <dave-koch@web.de>
2023-12-03 12:30:33 +0100

    adapt julia template to new interface

--- templates/jl/Solver.jl
+++ templates/jl/Solver.jl
@@ -4,18 +4,20 @@ using AoC
 using AoC.Utils
 
 
-function AoC.parse_input(raw_data)
+function parse_input(raw_data)
     raw_data
 end
 
 
-function AoC.solve1(parsed)
+function solve1(parsed)
 end
 
 
-function AoC.solve2(parsed)
+function solve2(parsed)
 end
 
+
+solution = Solution(parse_input, solve1, solve2)
 
 testinput = """
 """
@@ -23,8 +25,11 @@ testanswer_1 = nothing
 testanswer_2 = nothing
 export testinput, testanswer_1, testanswer_2
 
-test(part=missing) = AoC.test_solution(testinput, testanswer_1, testanswer_2, part)
+test() = AoC.test_solution(solution, testinput, testanswer_1, testanswer_2)
 export test
+
+main(part=missing) = AoC.main(solution, part)
+export main
 
 
 end # module Solver
