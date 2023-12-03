330f9b035d675af2f1d7960a475c0913d1977404
davekch <dave-koch@web.de>
2023-12-03 12:18:30 +0100

    julia: utils minor improvement

--- utils/jl/src/Utils.jl
+++ utils/jl/src/Utils.jl
@@ -9,7 +9,7 @@ export Geometry
 lines(s::AbstractString) = split(strip(s), "\n")
 export lines
 
-int(s::AbstractString)::Int = parse(Int, s)
+int(s)::Int = parse(Int, s)
 export int
 
 ints(s::AbstractString)::Vector{Int} = map(int, [s[idx] for idx in findall(r"-?[0-9]+", s)])
