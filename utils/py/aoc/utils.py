0a0f5812ac4a98eca7f78f2ffcfd95b5bdd6787f
davekch <dave-koch@web.de>
2023-12-26 02:49:13 +0100

    python utils: add dijkstra

--- utils/py/aoc/utils.py
+++ utils/py/aoc/utils.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from functools import wraps
-from queue import Queue
+from queue import Queue, PriorityQueue
+from collections import defaultdict
 from typing import Dict, Any
 
 from rich.traceback import install
@@ -43,6 +44,31 @@ def shortestpath(bfs_result, start, end) -> list:
         if parent == start:
             break
     return list(reversed(path))
+
+
+def dijkstra(graph, start, neighbors_func, distance_func, finished=None, visualize=None):
+    queue = PriorityQueue()
+    queue.put((0, start))
+    distances = defaultdict(lambda: 1000000000)
+    distances[start] = 0
+    path = {start: None}
+    while not queue.empty():
+        _, current = queue.get()
+
+        if visualize is not None:
+            visualize(graph, path, start, current)
+
+        if finished is not None and finished(current):
+            return current, distances, path
+        for n in neighbors_func(graph, current):
+            if n in path:
+                continue
+            d = distances[current] + distance_func(current, n, graph)
+            if d < distances[n]:
+                distances[n] = d
+                path[n] = current
+                queue.put((d, n))
+    return current, distances, path
 
 
 def corners(grid: Dict[tuple[int], Any]):
