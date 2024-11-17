775100cdebcd0c17660b351f52a5e1fe7431d715
davekch <dvkch@proton.me>
2024-11-17 17:18:25 +0100

    python utilities: add vector class

--- utils/py/src/aoc/geometry.py
+++ utils/py/src/aoc/geometry.py
@@ -3,3 +3,72 @@ def manhattan(p1, p2):
     manhattan distance between two n-D points of any iterable type
     """
     return sum([abs(x2 - x1) for x1, x2 in zip(p1, p2)])
+
+
+class Vec:
+    """
+    n-dimensional vectors
+
+        Vec(1,2) + Vec(3,4)
+        Vec(1,2,3) - Vec(5,6,7)
+        x, y = p1
+        p1.x, p1.y
+
+    """
+
+    def __init__(self, *coords):
+        self.coords = tuple(coords)
+
+    @property
+    def x(self):
+        return self.coords[0]
+
+    @property
+    def y(self):
+        return self.coords[1]
+
+    def __iter__(self):
+        return iter(self.coords)
+
+    def __add__(self, other):
+        return Vec(*[x1+x2 for x1, x2 in zip(self, other)])
+
+    def __sub__(self, other):
+        return Vec(*[x1-x2 for x1, x2 in zip(self, other)])
+
+    def __repr__(self):
+        return f"Vec({','.join([str(x) for x in self])})"
+
+    def abs2(self):
+        """squared euclidean distance"""
+        return sum([x**2 for x in self])
+
+    def dot(self, other):
+        return sum([x1 * x2 for x1, x2 in zip(self, other)])
+
+
+class Direction:
+    """
+    collection of discrete 2D direction vectors in a downwards-pointing coordinate system
+
+        ------> +x
+        |
+        |
+        v  +y
+
+    """
+    N = Vec(0, -1)
+    NE = Vec(1, -1)
+    E = Vec(1, 0)
+    SE = Vec(1, 1)
+    S = Vec(0, 1)
+    SW = Vec(-1, 1)
+    W = Vec(-1, 0)
+    NW  = Vec(-1, -1)
+
+
+def neighbours4(p: Vec) -> list[Vec]:
+    """
+    get the 4 horizontally and vertically adjacent points
+    """
+    return [p + d for d in [Direction.N, Direction.E, Direction.S, Direction.W]]
