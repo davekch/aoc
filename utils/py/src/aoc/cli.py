b6d59d4009dd7aa9e74133baf95f3b4ed1ba4a92
davekch <dvkch@proton.me>
2025-12-08 11:09:52 +0100

    py utils: accept solve function that solves both parts

--- utils/py/src/aoc/cli.py
+++ utils/py/src/aoc/cli.py
@@ -1,5 +1,6 @@
 from . import utils
 from argparse import ArgumentParser
+from types import GeneratorType
 
 
 def get_args():
@@ -9,13 +10,24 @@ def get_args():
     return parser.parse_args()
 
 
-def main(parse, solve1, solve2):
+def main(parse, solve1, solve2=None):
     args = get_args()
 
     watch = utils.stopwatch()
     data = watch.measure_time(parse)(
         open(args.input).read().strip()
     )
+    if not solve2:
+        # solve1 solves both parts
+        solution = watch.measure_time(solve1)(data)
+        # we accept either both results at the same time or a generator
+        if isinstance(solution, GeneratorType):
+            solve1 = solve2 = next
+            data = solution
+        else:
+            solve1 = lambda *_: solution[0]
+            solve2 = lambda *_: solution[1]
+
     if not args.part or args.part == 1:
         sol1 = watch.measure_time(solve1)(data)
         print(f"Part 1: {sol1}")
